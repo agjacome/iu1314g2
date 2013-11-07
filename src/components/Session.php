eef59fd72ce1aa5fc1e675bcefa81b49ba380ce5
Alberto G. Jácome <agjacome@gmail.com>
2013-11-07 02:12:16 +0100

    implementada clase Session ocultando la global $_SESSION

---
+++ src/components/Session.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace components;
+
+class Session
+{
+
+    public $sessionID;
+
+    public function __construct()
+    {
+        session_start();
+        $this->sessionID = session_id();
+    }
+
+    public function __destruct()
+    {
+        session_write_close();
+    }
+
+    public function destroy()
+    {
+        foreach ($_SESSION as $key => $value)
+            $_SESSION[$key] = null;
+
+        session_destroy();
+    }
+
+    public function __clone()
+    {
+        trigger_error("Clonacion no permitida para " . __CLASS__, E_USER_ERROR);
+    }
+
+    public function __get($key)
+    {
+        return $_SESSION[$key];
+    }
+
+    public function __set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+}
+
+?>
