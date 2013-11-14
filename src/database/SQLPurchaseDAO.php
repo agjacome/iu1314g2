b8966a436c10ca9726540c3edea172ff395103fc
Alberto G. Jácome <agjacome@gmail.com>
2013-11-14 21:20:06 +0100

    implementado DAO concreto para tabla COMPRA

---
+++ src/database/SQLPurchaseDAO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace database;
+
+class SQLBidDAO extends SQLDAO implements DAO
+{
+
+    public function __construct()
+    {
+        parent::__construct("COMPRA");
+    }
+
+}
+
+?>
