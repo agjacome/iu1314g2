339e9fb601fae4d38d8410662cbccf401ddd14f5
Daniel Álvarez Outerelo <danielalvarezouterelo@gmail.com>
2013-11-15 20:54:16 +0100

    Clase View FALTA PROBARLA

---
+++ src/views/View.php
@@ -0,0 +1,30 @@
+<?php
+	namespace views;
+
+	class View
+	{
+
+		private $template;
+		private $data;
+
+		public function __contruct($template)
+		{
+			$this->template = $template;
+			$this->data = array();
+		}
+
+		public  function render()
+		{
+			extract($this->data);
+			ob_start();
+			include $this->template;
+			$renderedView = ob_get_clean();
+			print $renderedView;
+		}
+
+		public function assign($key,$value)
+		{
+			$this->data[$key] = $value;
+		}
+	}
+?>
