d714e3900a89b1770da385300dcb7c303f2cb9fb
Alberto G. Jácome <agjacome@gmail.com>
2013-11-16 22:36:29 +0100

    reestructurada clase View, para poder cargar datos desde la sesion y lenguaje

--- src/views/View.php
+++ src/views/View.php
@@ -5,21 +5,25 @@ namespace views;
 class View
 {
 
-    //private $template;
     private $data;
+    private $session;
 
-    public function __construct()
+    public function __construct($session)
     {
         $this->data = array();
+        $this->session = $session;
     }
 
-    public  function render($template)
+    public function render($template)
     {
+        $this->loadData();
         extract($this->data);
+
         ob_start();
         include $template;
-        $renderedView = ob_get_clean();
-        print $renderedView;
+        $rendered = ob_get_clean();
+
+        print $rendered;
     }
 
     public function assign($key,$value)
@@ -27,5 +31,15 @@ class View
         $this->data[$key] = $value;
     }
 
+    private function loadData()
+    {
+        $this->data["lang"] = \components\Language::getStrings($this->session);
+        $this->data["logged_in"] = isset($this->session->logged_in) ? $this->session->logged_in : false;
+
+        if (isset($this->session->username)) $this->data["username"] = $this->session->username;
+        if (isset($this->session->userrole)) $this->data["userrole"] = $this->session->userrole;
+    }
+
 }
+
 ?>
