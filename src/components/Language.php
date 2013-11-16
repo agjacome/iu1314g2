51d2bf47af6b89772bfc9216e38bfa436e1519c2
Alberto G. Jácome <agjacome@gmail.com>
2013-11-16 13:45:22 +0100

    arreglado getStrings en Language

--- src/components/Language.php
+++ src/components/Language.php
@@ -6,15 +6,18 @@ class Language
 {
 
     private static $strings;
-    private static $language = Configuration::getValue("language", "default");
+    private static $language;
 
     public static function getStrings($session = null)
     {
+        if (!isset(self::$language))
+            self::$language = Configuration::getValue("language", "default");
+
         if (isset($session->lang) && self::$language !== $session->lang)
             self::$strings = null;
 
         if (!isset(self::$strings))
-            return parse_ini_file(__DIR__ . "/../lang/" . $language . ".ini", true);
+            return parse_ini_file(__DIR__ . "/../lang/" . self::$language . ".ini", true);
 
         return $strings;
     }
