730fad56e6ab02fba2bd30d17552f6eaf75fa2a0
Alberto G. Jácome <agjacome@gmail.com>
2013-11-17 04:04:05 +0100

    implementacion de cambio de idioma en HomeController

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -4,14 +4,24 @@ namespace controllers;
 
 class HomeController extends Controller
 {
-	public function __construct($request)
-	{
-		parent::__construct($request);
-	}
 
-	public function defaultAction()
-	{
-	} 
+    public function __construct($request)
+    {
+        parent::__construct($request);
+    }
+
+    public function defaultAction()
+    {
+        // FIXME: hack temporal para pruebas
+        $this->redirect("user");
+    }
+
+    public function changeLanguage()
+    {
+        $this->session->lang = $this->request->lang;
+        $this->redirect();
+    }
+
 }
 
 ?>
