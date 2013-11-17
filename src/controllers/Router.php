313e844edbbedc2498e93418e3f719405768c25d
Alberto G. Jácome <agjacome@gmail.com>
2013-11-17 04:01:07 +0100

    limpieza de codigo y adecuacion a normativa en Router

--- src/controllers/Router.php
+++ src/controllers/Router.php
@@ -4,33 +4,26 @@ namespace controllers;
 
 class Router
 {
-	public function routeRequest($request)
-	{
-		if (!(isset($request->controller))) {
-			$controller = "controllers\\HomeController";
-		} else {
-			$controller = $request->controller;
-			$controller = "controllers\\" . (ucfirst($controller) . 'sController');
-		}
 
-		if (isset($request->action)) {
-			$action = $request->action;
-		} else {
-			$action = "defaultAction";
-		}
+    public function routeRequest($request)
+    {
+        $controller = "controllers\\HomeController";
+        if ((isset($request->controller))) {
+            $controller = $request->controller;
+            $controller = "controllers\\" . ucfirst($controller) . "sController";
+        }
 
-		if ( class_exists($controller) && method_exists($controller,$action) )
-		{
-			$controller = new $controller($request);
-			$controller->$action();
-		} else {
-			print("Pagina no encontratada.");
-		}
-			/*
-			$view = new \views\View("404.html");
-			$view->render();
-			*/
-	}
+        $action = isset($request->action) ? $request->action : "defaultAction";
+
+        if (class_exists($controller) && method_exists($controller,$action)) {
+            $controller = new $controller($request);
+            $controller->$action();
+        } else {
+            // FIXME: hack temporal, deberia invocarse a una vista
+            print "Pagina no encontrada.";
+        }
+    }
+
 }
 
 ?>
