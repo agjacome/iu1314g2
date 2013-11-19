0d87cfe7d63a37ca8a03cc170d5f207722b1884a
Alberto G. Jácome <agjacome@gmail.com>
2013-11-19 16:51:37 +0100

    integracion temporal para pruebas de plantillas

--- src/views/templates/sidebar.php
+++ src/views/templates/sidebar.php
@@ -1,20 +1,9 @@
-<?php 
-include cabecera.php ?> 
-
-
-
 <div class="left_content">
     <div class="title_box"><?php print $lang ["head"]["advanced_search"]; ?></div>
-    
-        <ul class="left_menu">
-        <li class="odd"><a href="registro.php"><?php print $lang ["head"]["registro"]; ?></a></li>
-        <li class="even"><a href="logout.php"><?php print $lang ["head"]["cerrar_sesion"]; ?></a></li>
-         <li class="odd"><a href="subastasactivas.php"><?php print $lang ["head"]["subastas"]; ?></a></li>
-        <li class="even"><a href="productos.php"><?php print $lang ["head"]["productos"]; ?></a></li>
-       
-        </ul> 
-        
-      
-        
-    
-   </div><!-- end of left content -->
+    <ul class="left_menu">
+        <li class="odd"><a href="/index.php?controller=user&action=create"><?php print $lang ["head"]["registro"]; ?></a></li>
+        <li class="even"><a href="/index.php?controller=user&action=logout"><?php print $lang ["head"]["cerrar_sesion"]; ?></a></li>
+        <li class="odd"><a href="#"><?php print $lang ["head"]["subastas"]; ?></a></li>
+        <li class="even"><a href="#"><?php print $lang ["head"]["productos"]; ?></a></li>
+    </ul>
+</div><!-- end of left content -->
