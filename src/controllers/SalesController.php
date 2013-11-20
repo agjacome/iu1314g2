4adb53ba68a9e8493ee0e14b906b28a728ecc606
Alberto G. Jácome <agjacome@gmail.com>
2013-11-20 16:35:19 +0100

    implementada consulta y listado de ventas

--- src/controllers/SalesController.php
+++ src/controllers/SalesController.php
@@ -17,11 +17,11 @@ class SalesController extends Controller
     private $sale;    // modelo de venta, se instanciara cuando resulte necesario
 
     /**
-     * Constructor, construye la instancia de Controller a partir de la 
+     * Constructor, construye la instancia de Controller a partir de la
      * peticion recibida.
      *
      * @param \components\Request $request
-     *     Peticion HTTP recibida, encapsulada dentro de un objeto Request (ver 
+     *     Peticion HTTP recibida, encapsulada dentro de un objeto Request (ver
      *     en namespace components).
      */
     public function __construct($request)
@@ -39,8 +39,8 @@ class SalesController extends Controller
     }
 
     /**
-     * Crea una nueva venta asociada a un producto dado. Solo se permite 
-     * creacion de venta al usuario propietario del producto y/o al 
+     * Crea una nueva venta asociada a un producto dado. Solo se permite
+     * creacion de venta al usuario propietario del producto y/o al
      * administrador.
      */
     public function create()
@@ -60,14 +60,14 @@ class SalesController extends Controller
             $this->redirect("product");
         }
 
-        // solo se permite la puesta en venta al propietario o a un 
+        // solo se permite la puesta en venta al propietario o a un
         // administrador
         if ($this->session->username !== $this->product->getOwner() && !$this->isAdmin()) {
             $this->setFlash($this->lang["sale"]["create_err"]);
             $this->redirect("sale");
         }
 
-        // si GET, redirige al formulario de creacion de venta, con los datos 
+        // si GET, redirige al formulario de creacion de venta, con los datos
         // necesarios del producto
         if ($this->request->isGet()) {
             $this->view->assign("product", $this->product);
@@ -95,7 +95,7 @@ class SalesController extends Controller
         $required = isset($this->request->price) && isset($this->request->stock);
         if (!$required) return false;
 
-        // crea venta, actualiza estado de producto, valida campos e inserta en 
+        // crea venta, actualiza estado de producto, valida campos e inserta en
         // BD
         $this->sale = new \models\Sale(null, $this->product->getId());
         $this->sale->price    = $this->request->price;
@@ -103,16 +103,16 @@ class SalesController extends Controller
         $this->product->state = "venta";
 
         return $this->sale->validate() && $this->product->validate() &&
-               $this->sale->save() && $this->product->save();
+            $this->sale->save() && $this->product->save();
     }
 
     /**
-     * Modifica los datos almacenados de una venta. Solo se permite 
+     * Modifica los datos almacenados de una venta. Solo se permite
      * modificacion al propietario del producto en venta y/o al administrador.
      */
     public function update()
     {
-        // bajo ninguna circunstancia se permite modificacion de ventas sin 
+        // bajo ninguna circunstancia se permite modificacion de ventas sin
         // estar identificado en el sistema
         if (!$this->isLoggedIn())
             $this->redirect("user", "login");
@@ -129,14 +129,14 @@ class SalesController extends Controller
             $this->redirect("sale");
         }
 
-        // solo se permite modificacion de venta al propietario del producto 
+        // solo se permite modificacion de venta al propietario del producto
         // y/o a un administrador
         if ($this->session->username !== $this->product->getOwner() && !$this->isAdmin()) {
             $this->setFlash($this->lang["sale"]["update_err"]);
             $this->redirect("sale");
         }
 
-        // si GET, redirige al formulario de modificacion, que recibe todos los 
+        // si GET, redirige al formulario de modificacion, que recibe todos los
         // datos actuales del producto y la venta
         if ($this->request->isGet()) {
             $this->view->assign("product" , $this->product);
@@ -161,8 +161,8 @@ class SalesController extends Controller
      */
     private function updatePost()
     {
-        // para los dos campos modificables desde el formulario (precio y 
-        // stock, puesto que el resto son inmutables), comprueba que no lleguen 
+        // para los dos campos modificables desde el formulario (precio y
+        // stock, puesto que el resto son inmutables), comprueba que no lleguen
         // vacios, si es asi no modifica el contenido previo
         if (!empty($this->request->price))
             $this->sale->price = $this->request->price;
@@ -174,13 +174,13 @@ class SalesController extends Controller
     }
 
     /**
-     * Elimina una venta, estableciendo del producto de nuevo a pendiente. Solo 
-     * se permite la eliminacion de la venta al propietario del producto y/o al 
+     * Elimina una venta, estableciendo del producto de nuevo a pendiente. Solo
+     * se permite la eliminacion de la venta al propietario del producto y/o al
      * administrador.
      */
     public function delete()
     {
-        // en ningun caso se permite la eliminacion de ventas sin estar 
+        // en ningun caso se permite la eliminacion de ventas sin estar
         // identificado en el sistema
         if (!$this->isLoggedIn())
             $this->redirect("user", "login");
@@ -201,14 +201,14 @@ class SalesController extends Controller
             $this->redirect("sale");
         }
 
-        // solo el propietario del producto en venta y/o un administrador 
+        // solo el propietario del producto en venta y/o un administrador
         // podran eliminar la venta
         if ($this->product->getOwner() !== $this->session->username && !$this->isAdmin()) {
             $this->setFlash($this->lang["sale"]["delete_err"]);
             $this->redirect("sale");
         }
 
-        // elimina la venta, si es posible, actualizando el estado del producto 
+        // elimina la venta, si es posible, actualizando el estado del producto
         // de nuevo a "pendiente" y redirecciona acordemente
         $this->product->state = "pendiente";
         if ($this->product->validate() && $this->product->save() && $this->sale->delete()) {
@@ -225,7 +225,27 @@ class SalesController extends Controller
      */
     public function get()
     {
-        trigger_error("Aun no implementado", E_USER_ERROR);
+        // se debe proporcionar el identificador de la venta a consultar
+        if (!isset($this->request->id))
+            $this->redirect("sale");
+
+        // obtiene datos de la venta y el producto
+        $this->sale = new \models\Sale($this->request->id);
+        if (!$this->sale->fill()) {
+            $this->setFlash($this->lang["sale"]["get_err"]);
+            $this->redirect("sale");
+        }
+        $this->product = new \models\Product($this->sale->getProductId());
+        if (!$this->product->fill()) {
+            $this->setFlash($this->lang["sale"]["get_err"]);
+            $this->redirect("sale");
+        }
+
+        // se le pasan los datos de la venta y producto a la vista, y se
+        // renderiza
+        $this->view->assign("product" , $this->product);
+        $this->view->assign("sale"    , $this->sale);
+        $this->view->render("sale_get");
     }
 
     /**
@@ -233,12 +253,31 @@ class SalesController extends Controller
      */
     public function listing()
     {
-        trigger_error("Aun no implementado", E_USER_ERROR);
+        // obtiene todas las ventas del sistema
+        $sales = \models\Sale::findBy(null);
+
+        // se crea un array donde cada campo sera un par (venta, producto),
+        // recuperando para ello los datos del producto en cada una de las
+        // ventas obtenidas en la instruccion anterior
+        $list = array();
+        foreach ($sales as $sale) {
+            $product = new \models\Product($sale->getProductId());
+            if (!$product->fill()) break;
+
+            $list[ ] = [
+                "sale"    => $sale,
+                "product" => $product
+            ];
+        }
+
+        // se devuelve el array de pares creado
+        $this->view->assign("list", $list);
+        $this->view->render("sale_list");
     }
 
     /**
-     * Proporciona un listado de todos los productos en venta por los cuales el 
-     * usuario identificado, o bien uno dado si invocado por administrador, ha 
+     * Proporciona un listado de todos los productos en venta por los cuales el
+     * usuario identificado, o bien uno dado si invocado por administrador, ha
      * pujado.
      */
     public function purchased()
@@ -247,8 +286,8 @@ class SalesController extends Controller
     }
 
     /**
-     * Realiza una compra de un producto en venta y su pago asociado. Si la 
-     * compra no es pagada, no se almacenara en la BD y, por tanto, quedara 
+     * Realiza una compra de un producto en venta y su pago asociado. Si la
+     * compra no es pagada, no se almacenara en la BD y, por tanto, quedara
      * descartada. No se consideran "compras pendientes de pago".
      */
     public function purchase()
