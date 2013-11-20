0663edf1b0c4dd7c507b96e7d29a6967f6622cd7
Alberto G. Jácome <agjacome@gmail.com>
2013-11-20 15:47:04 +0100

    implementada modificacion de ventas

--- src/controllers/SalesController.php
+++ src/controllers/SalesController.php
@@ -81,6 +81,7 @@ class SalesController extends Controller
                 $this->redirect("sale");
             } else {
                 $this->setFlash($this->lang["sale"]["create_err"]);
+                $this->redirect("sale", "create");
             }
         }
     }
@@ -94,12 +95,15 @@ class SalesController extends Controller
         $required = isset($this->request->price) && isset($this->request->stock);
         if (!$required) return false;
 
-        // crea venta, valida campos e inserta a la BD
+        // crea venta, actualiza estado de producto, valida campos e inserta en 
+        // BD
         $this->sale = new \models\Sale(null, $this->product->getId());
-        $this->sale->price = $this->request->price;
-        $this->sale->stock = $this->request->stock;
+        $this->sale->price    = $this->request->price;
+        $this->sale->stock    = $this->request->stock;
+        $this->product->state = "venta";
 
-        return $this->sale->validate() && $this->sale->save();
+        return $this->sale->validate() && $this->product->validate() &&
+               $this->sale->save() && $this->product->save();
     }
 
     /**
@@ -108,7 +112,65 @@ class SalesController extends Controller
      */
     public function update()
     {
-        trigger_error("Aun no implementado", E_USER_ERROR);
+        // bajo ninguna circunstancia se permite modificacion de ventas sin 
+        // estar identificado en el sistema
+        if (!$this->isLoggedIn())
+            $this->redirect("user", "login");
+
+        // se debe proporcionar el identificador de venta a modificar
+        if (!isset($this->request->id))
+            $this->redirect("sale");
+
+        // la venta a modificar debe existir previamente en la BD
+        $this->sale    = new \models\Sale($this->request->id);
+        $this->product = new \models\Product($this->sale->getProductId());
+        if (!$this->sale->fill() || !$this->product->fill()) {
+            $this->setFlash($this->lang["sale"]["update_err"]);
+            $this->redirect("sale");
+        }
+
+        // solo se permite modificacion de venta al propietario del producto 
+        // y/o a un administrador
+        if ($this->session->username !== $this->product->getOwner() && !$this->isAdmin()) {
+            $this->setFlash($this->lang["sale"]["update_err"]);
+            $this->redirect("sale");
+        }
+
+        // si GET, redirige al formulario de modificacion, que recibe todos los 
+        // datos actuales del producto y la venta
+        if ($this->request->isGet()) {
+            $this->view->assign("product" , $this->product);
+            $this->view->assign("sale"    , $this->sale);
+            $this->view->render("sale_update");
+        }
+
+        // si POST, realiza la modificacion y redirige
+        if ($this->request->isPost()) {
+            if ($this->updatePost()) {
+                $this->setFlash($this->lang["sale"]["update_ok"]);
+                $this->redirect("sale");
+            } else {
+                $this->setFlash($this->lang["sale"]["update_err"]);
+                $this->redirect("sale", "update");
+            }
+        }
+    }
+
+    /**
+     * Metodo privado interno para el manejo de la peticion POST en update()
+     */
+    private function updatePost()
+    {
+        // para los dos campos modificables desde el formulario (precio y 
+        // stock, puesto que el resto son inmutables), comprueba que no lleguen 
+        // vacios, si es asi no modifica el contenido previo
+        if (!empty($this->request->price))
+            $this->sale->price = $this->request->price;
+        if (!empty($this->request->stock))
+            $this->sale->stock = $this->request->stock;
+
+        // valida campos y almacena en BD
+        return $this->sale->validate() && $this->sale->save();
     }
 
     /**
