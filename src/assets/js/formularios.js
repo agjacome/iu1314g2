470785f029e9e3b91f5a65ae8fef5d8ff48a28d8
Alberto G. Jácome <agjacome@gmail.com>
2013-11-21 01:35:37 +0100

    actualizados assets e idiomas a version realizada por el Equipo A

---
+++ src/assets/js/formularios.js
@@ -0,0 +1,9 @@
+ //JavaScript Document
+function insprodd(){ 
+	if(confirm('¿Estás seguro de modificar este usuario?')){
+       document.insprod.submit();
+    } 
+    else{
+    	alert('Pues fijese más y no sea estúpido');
+    }
+} 
