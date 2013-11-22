56d77f0b35ffe8926d2714503defdd6ce497535f
Alberto G. Jácome <agjacome@gmail.com>
2013-11-22 14:17:54 +0100

    integracion parcial de plantillas de subasta

--- src/views/templates/bidding_get.php
+++ src/views/templates/bidding_get.php
@@ -1,1 +1,64 @@
-<h1>NO IMPLEMENTADO</h1>
+<div class="center_content">
+    <div class="center_title_bar"><?php print $lang["bidding"]["details"]; ?></div>
+
+    <ul class="list">
+        <li class="even"><?php print "<strong>" . $lang["product"]["name"]      . ":</strong> " . $product->name; ?></li>
+        <li class="even"><?php print "<strong>" . $lang["product"]["owner"]     . ":</strong> " . $product->getOwner(); ?></li>
+        <li class="even"><?php print "<strong>" . $lang["product"]["descr"]     . ":</strong> " . $product->description; ?></li>
+        <li class="even"><?php print "<strong>" . $lang["product"]["rateAvg"]   . ":</strong> " . $rate; ?> / 5</li>
+        <li class="even"><?php print "<strong>" . $lang["bidding"]["minBid"]    . ":</strong> " . $bidding->minBid; ?></li>
+        <li class="even"><?php print "<strong>" . $lang["bidding"]["limitDate"] . ":</strong> " . date("d-m-Y", strtotime($bidding->limitDate)); ?></li>
+        <li class="odd">
+<?php if ($this->isLoggedIn()) { ?>
+            <a href="/index.php?controller=sale&action=purchase&sale=<?php print $sale->getId(); ?>">
+                <?php print $lang["sale"]["buy"]; ?>
+            </a>
+            &nbsp;|&nbsp;
+            <a href="/index.php?controller=product&action=rate&prod=<?php print $product->getId(); ?>">
+                <?php print $lang["product"]["rate"]; ?>
+            </a>
+<?php   if ($this->session->username === $product->getOwner() || $this->isAdmin()) { ?>
+            &nbsp;|&nbsp;
+            <a href="/index.php?controller=sale&action=update&id=<?php print $sale->getId(); ?>">
+                <?php print $lang["sale"]["update"]; ?>
+            </a>
+            &nbsp;|&nbsp;
+            <a href="/index.php?controller=sale&action=delete&id=<?php print $sale->getId(); ?>">
+                <?php print $lang["sale"]["delete"]; ?>
+            </a>
+<?php
+        }
+      } else {
+?>
+            <a href="/index.php?controller=user&action=login">
+                <?php print $lang["sale"]["login_to_buy"]; ?>
+            </a>
+<?php } ?>
+        </li>
+    </ul>
+
+    <div class="center_title_bar"><?php print $lang["product"]["ratings"]; ?></div>
+    <ul class="list">
+<?php
+
+    if (count($ratings) > 0) {
+        foreach ($ratings as $rating) {
+?>
+        <li class="even">
+            <?php print "<strong>" . $lang["product"]["author"]  . ":</strong> " . $rating->getLogin(); ?>
+            <br />
+            <?php print "<strong>" . $lang["product"]["rating"]  . ":</strong> " . $rating->rating; ?>
+            <br />
+            <?php print "<strong>" . $lang["product"]["comment"] . ":</strong> " . $rating->commentary; ?>
+        </li>
+<?php
+        }
+    } else {
+?>
+        <li class="even">
+            <?php print $lang["product"]["no_ratings"]; ?>
+        </li>
+<?php } ?>
+    </ul>
+
+</div> <!-- end of center content -->
