f7c68b3d733c6f7677916ebd416c89fbdd172d72
Alberto G. Jácome <agjacome@gmail.com>
2013-11-22 11:17:08 +0100

    incluidos links para poner en venta/subasta un producto pendiente en la plantilla de datos de producto

--- src/views/templates/product_get.php
+++ src/views/templates/product_get.php
@@ -4,6 +4,7 @@
     <ul class="list">
         <li class="even"><?php print "<strong>" . $lang["product"]["name"]    . ":</strong> " . $product->name; ?></li>
         <li class="even"><?php print "<strong>" . $lang["product"]["owner"]   . ":</strong> " . $product->getOwner(); ?></li>
+        <li class="even"><?php print "<strong>" . $lang["product"]["state"]   . ":</strong> " . $product->state; ?></li>
         <li class="even"><?php print "<strong>" . $lang["product"]["descr"]   . ":</strong> " . $product->description; ?></li>
         <li class="even"><?php print "<strong>" . $lang["product"]["rateAvg"] . ":</strong> " . $rateAvg; ?> / 5</li>
         <li class="odd">
@@ -20,7 +21,16 @@
             <a href="/index.php?controller=product&action=delete&id=<?php print $product->getId(); ?>">
                 <?php print $lang["product"]["delete"]; ?>
             </a>
-<?php } } ?>
+<?php        if ($product->state === "pendiente") { ?>
+            &nbsp;|&nbsp;
+            <a href="/index.php?controller=sale&action=create&product=<?php print $product->getId(); ?>">
+                <?php print $lang["product"]["sale"]; ?>
+            <a>
+            &nbsp;|&nbsp;
+            <a href="/index.php?controller=bidding&action=create&product=<?php print $product->getId(); ?>">
+                <?php print $lang["product"]["auction"]; ?>
+            </a>
+<?php } } } ?>
         </li>
     </ul>
 
