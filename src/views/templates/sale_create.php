b9af35e33c08f5fdb5f1d02ca283dd47321dc8b5
Alberto G. Jácome <agjacome@gmail.com>
2013-11-22 10:08:54 +0100

    integrada plantilla de creacion de ventas de productos

--- src/views/templates/sale_create.php
+++ src/views/templates/sale_create.php
@@ -1,1 +1,29 @@
-<h1>NO IMPLEMENTADO</h1>
+<div class="contact_form">
+<div class="center_title_bar"><?php print $lang["sale"]["create"]; ?></div>
+    <div class="prod_box_big">
+        <div class="top_prod_box_big"></div>
+        <div class="center_prod_box_big">
+            <div class="contact_form">
+                <form action="index.php" method="post" accept-charset="utf-8">
+                    <input type="hidden" name="controller" value="sale" />
+                    <input type="hidden" name="action"     value="create" />
+                    <input type="hidden" name="product"    value="<?php print $product->getId(); ?>" />
+
+                    <div class="form_row">
+                        <label class="contact"><strong><?php print $lang["sale"]["price"]; ?></strong></label>
+                        <input type="text" name="price" class="contact_input" />
+                    </div>
+
+                    <div class="form_row">
+                        <label class="contact"><strong><?php print $lang["sale"]["stock"]; ?></strong></label>
+                        <input type="text" name="stock" class="contact_input" />
+                    </div>
+
+                    <div class="form_row">
+                        <input type="submit" value="<?php print $lang["general"]["accept"]; ?>" class="contact">
+                    </div>
+                </form>
+            </div>
+        <div class="bottom_prod_box_big"></div>
+    </div>
+</div><!-- end of center content -->
