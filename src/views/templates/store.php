680446656a4fb1f145ba004792339c882728c38b
Alberto G. Jácome <agjacome@gmail.com>
2013-11-23 14:25:36 +0100

    eliminadas plantillas innecesarias

--- src/views/templates/store.php
+++ src/views/templates/store.php
@@ -4,6 +4,5 @@
         <li class="even"><a href="/index.php?controller=user&action=listing"><?php print $lang["user"]["list"]; ?></a></li>
         <li class="odd"><a href="/index.php?controller=product&action=listing"><?php print $lang["product"]["list"]; ?></a></li>
         <li class="even"><a href="/index.php?controller=store&action=changeCommission"><?php print $lang["store"]["changecomm"]; ?></a></li>
-        <li class="odd"><a href="/index.php?controller=store&action=stats"><?php print $lang["store"]["stats"]; ?></a></li>
     </ul>
 </div>
