e8f75d9f02a0e6d04175b5b3395fbbb8eca7cfc8
Alberto G. Jácome <agjacome@gmail.com>
2013-11-23 22:10:21 +0100

    integrada plantilla de ayuda

---
+++ src/views/templates/help.php
@@ -0,0 +1,57 @@
+<div class="center_content">
+<div class="center_title_bar"><?php print $lang ["ayuda"]["ayuda"]; ?></div>    
+<div class="text_center">
+    <ul>
+        <li>
+            <?print $lang ["ayuda"]["comprar"]; ?>
+        </li>
+
+        <li>
+            <?print $lang ["ayuda"]["pujar"]; ?>
+        </li>
+
+        <li>
+            <?print $lang ["ayuda"]["insertArticulo"]; ?>
+        </li>
+
+        <li>
+            <?print $lang ["ayuda"]["eliminarProduct"]; ?>
+        </li>
+
+        <li>
+            <?print $lang ["ayuda"]["modifyProduct"]; ?>
+        </li>
+
+        <li>
+            <?print $lang["ayuda"]["modifyUser"]; ?>
+
+        </li>
+
+        <li>
+            <?print $lang["ayuda"]["misCompras"]; ?>
+
+        </li>
+
+        <li>
+            <?print $lang["ayuda"]["misVentas"]; ?>
+
+        </li>
+
+        <li>
+            <?print $lang["ayuda"]["estadistic"]; ?>
+
+        </li>
+
+        <li>
+            <?print $lang["ayuda"]["eliminUser"]; ?>
+        </li>
+
+        <li>
+            <?print $lang["ayuda"]["busq"]; ?>
+        </li>
+
+
+    </ul>
+</div>
+
+</div><!-- end of center content -->
